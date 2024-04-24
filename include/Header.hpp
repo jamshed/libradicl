c559df736eb0dae6c9ebd12534a7290c96665ef2
jamshed <jamshed@umd.edu>
2024-04-24 15:57:36 -0400

    Use allowed types only in header

--- include/Header.hpp
+++ include/Header.hpp
@@ -4,6 +4,8 @@
 
 
 
+#include "Type.hpp"
+
 #include <cstdint>
 #include <vector>
 #include <string>
@@ -17,14 +19,14 @@ class Header
 {
 private:
 
-    const bool is_paired;   // Whether the mappings to follow for paired-end or single-end fragments.
-    const uint64_t ref_count;   // Number of reference sequences being aligned against.
-    std::vector<std::string> ref_names; // Strings encoding the names of the target references.
-    uint64_t num_chunks;    // Number of chunks in the file following the header. If this value is 0, then the number of chunks is not known.
+    const Type::b is_paired;    // Whether the mappings to follow for paired-end or single-end fragments.
+    const Type::u64 ref_count;  // Number of reference sequences being aligned against.
+    std::vector<Type::str> ref_names;   // Strings encoding the names of the target references.
+    Type::u64 num_chunks;   // Number of chunks in the file following the header. If this value is 0, then the number of chunks is not known.
 
 public:
 
-    explicit Header(bool is_paired, uint64_t ref_count, const std::vector<std::string>& ref_names, uint64_t num_chunks = 0);
+    explicit Header(bool is_paired, uint64_t ref_count, const std::vector<std::string>& refs, uint64_t num_chunks = 0);
 
     Header(const Header&) = default;
     Header(Header&&) = default;
--- src/Header.cpp
+++ src/Header.cpp
@@ -1,15 +1,22 @@
 
 #include "Header.hpp"
 
+#include <cassert>
+
 
 namespace RAD
 {
 
-Header::Header(const bool is_paired, const uint64_t ref_count, const std::vector<std::string>& ref_names, const uint64_t num_chunks):
+Header::Header(const bool is_paired, const uint64_t ref_count, const std::vector<std::string>& refs, const uint64_t num_chunks):
       is_paired(is_paired)
     , ref_count(ref_count)
-    , ref_names(ref_names)
     , num_chunks(num_chunks)
-{}
+{
+    ref_names.reserve(ref_count);
+    for(const auto& v : refs)
+        ref_names.emplace_back(v);
+
+    assert(ref_names.size() == ref_count);
+}
 
 }
