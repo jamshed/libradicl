b3549d18da9b104e6ed8563d83298aed9d000ea3
jamshed <jamshed@umd.edu>
2024-04-28 22:43:02 -0400

    Support paired-end read

--- include/Read_Record.hpp
+++ include/Read_Record.hpp
@@ -17,20 +17,17 @@
 namespace RAD
 {
 
-class Single_End_Read
+class Read
 {
 
-private:
+protected:
 
     Type::u32 aln_count;    // Number of alignment records to follow for this read.
-    Type::u16 read_len; // Currently don’t support reads > 65,536bp.
 
     Byte_Array byte_arr;    // Container for tag-values and alignment records.
 
 
 public:
-
-    void set(uint32_t aln_count, uint16_t read_len);
 
     template <typename T_tag_>
     void add_tag(const T_tag_& val);
@@ -45,6 +42,58 @@ public:
 };
 
 
+class Single_End_Read : public Read
+{
+
+private:
+
+    Type::u16 read_len; // Currently don’t support reads > 65,536bp.
+
+
+public:
+
+    void set(uint32_t aln_count, uint16_t read_len);
+};
+
+
+class Paired_End_Read : public Read
+{
+
+private:
+
+    Type::u16 read_l_end_len;
+    Type::u16 read_r_end_len;
+
+
+public:
+
+    void set(uint32_t aln_count, uint16_t read_l_end_len, uint16_t read_r_end_len);
+
+};
+
+
+template <typename T_tag_>
+inline void Read::add_tag(const T_tag_& val)
+{
+    static_assert(is_RAD_type<T_tag_>());
+    assert(byte_arr.empty());
+
+    byte_arr.add(val);
+}
+
+
+inline void Read::add_aln_rec(const Aln_Record& aln_rec)
+{
+    aln_rec.dump(byte_arr);
+}
+
+
+inline void Read::dump(Buffer& buf) const
+{
+    buf.add(byte_arr);
+}
+
+
 inline void Single_End_Read::set(const uint32_t aln_count, const uint16_t read_len)
 {
     this->aln_count = aln_count;
@@ -56,25 +105,16 @@ inline void Single_End_Read::set(const uint32_t aln_count, const uint16_t read_l
 }
 
 
-template <typename T_tag_>
-inline void Single_End_Read::add_tag(const T_tag_& val)
+inline void Paired_End_Read::set(const uint32_t aln_count, const uint16_t read_l_end_len, const uint16_t read_r_end_len)
 {
-    static_assert(is_RAD_type<T_tag_>());
-    assert(byte_arr.empty());
+    this->aln_count = aln_count;
+    this->read_l_end_len = read_l_end_len;
+    this->read_r_end_len = read_r_end_len;
 
-    byte_arr.add(val);
-}
-
-
-inline void Single_End_Read::add_aln_rec(const Aln_Record& aln_rec)
-{
-    aln_rec.dump(byte_arr);
-}
-
-
-inline void Single_End_Read::dump(Buffer& buf) const
-{
-    buf.add(byte_arr);
+    byte_arr.clear();
+    byte_arr.add(this->aln_count);
+    byte_arr.add(this->read_l_end_len);
+    byte_arr.add(this->read_r_end_len);
 }
 
 }
